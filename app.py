892bde9e48644fe1f6faafbee1a6954ee6d7d64c
REDDY9398 <yashaswireddy879@gmail.com>
2026-03-27 08:56:46 +0530

    deploy update

--- app.py
+++ app.py
@@ -177,6 +177,6 @@ def forgot():
 # Run app
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()   # creates DB + table
+        db.create_all()
 
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=10000)
